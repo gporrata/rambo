629a9692ee2b2f91ec1c93cb57034c96a5e6d30f
George Porrata <gporrata@gmail.com>
2021-05-01 13:53:43 -0400

    refactor: any() without temp variable

--- any.v
+++ any.v
@@ -1,12 +1,10 @@
 module rambo
 
 pub fn any<T>(predicate fn(T) bool, list []T) bool {
-	mut passed := false
 	for element in list {
 		if predicate(element) {
-			passed = true
-			break
+			return true
 		}
 	}
-	return passed
+	return false
 }
