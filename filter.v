978d212f228fb963d44a0418ba05dea44a18522a
George Porrata <gporrata@gmail.com>
2021-05-02 13:49:15 -0400

    doc: show where one should use builtin .map, .filter

--- filter.v
+++ filter.v
@@ -1,6 +1,7 @@
 module rambo
 
 pub fn filter<T>(predicate fn(T) bool, list []T) []T {
+	// TODO: use builtin list.filter
 	mut result := []T{cap: list.cap}
 	for element in list {
 		if predicate(element) {
--- map.v
+++ map.v
@@ -1,6 +1,7 @@
 module rambo
 
 pub fn map<T, U>(predicate fn(T) U, list []T) []U {
+	// TODO: use builtin list.map
 	mut result := []U{cap: list.cap}
 	for element in list {
 		result << predicate(element)
