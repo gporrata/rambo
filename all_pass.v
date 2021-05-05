94050204852327c7842b38565b40c2b45f379013
George Porrata <gporrata@gmail.com>
2021-05-05 00:20:03 -0400

    doc: unable to make all_pass generic

--- all_pass.v
+++ all_pass.v
@@ -1,5 +1,6 @@
 module rambo
 
+// TODO: making this generic fails. should be able to use T for all ints
 pub fn all_pass(predicates []fn(val int) bool, input int) bool {
 	for predicate in predicates {
 		if ! predicate(input) { return false }
