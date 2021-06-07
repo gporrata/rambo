52c667ef1335ca5bafa89a4059b42f1b9637ea56
George Porrata <gporrata@gmail.com>
2021-06-07 05:38:46 -0400

    feat: find_index now generic

--- find_index.v
+++ find_index.v
@@ -1,6 +1,6 @@
 module rambo
 
-pub fn find_index(predicate fn(n int) bool, list []int) int {
+pub fn find_index<T>(predicate fn(n T) bool, list []T) int {
 	for i := 0; i < list.len; i++ {
 		if predicate(list[i]) { return i }
 	}
