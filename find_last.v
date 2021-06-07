55d21057a727d9c3bd929c894976203a7b3df3d4
George Porrata <gporrata@gmail.com>
2021-06-07 05:58:23 -0400

    feat: find_last now generic

--- find_last.v
+++ find_last.v
@@ -1,6 +1,6 @@
 module rambo
 
-pub fn find_last(predicate fn(n int) bool, list []int) ?int {
+pub fn find_last<T>(predicate fn(n T) bool, list []T) ?T {
 	for i := list.len - 1; i >= 0; i-- {
 		if predicate(list[i]) { return list[i] }
 	}
