423ee7273424d5380175c2cd8f94b430634cac21
George Porrata <george.porrata@tstllc.net>
2023-06-19 22:09:55 -0400

    feat: curry1of3

--- curryN.v
+++ curryN.v
@@ -5,10 +5,17 @@ pub fn curry1of2<A, B, Z>(
 	arg0 A
 ) fn(B) Z
 {
-	return fn[predicate, arg0] <B, Z> (arg1 B) Z {
+	return fn[predicate, arg0] <A, B, Z> (arg1 B) Z {
 		return predicate(arg0, arg1)
 	}
 }
 
-// waiting for this to compile successfuly. vlang bug
-// https://github.com/vlang/v/issues/17749
+pub fn curry1of3<A, B, D, Z>(
+	predicate fn(arg0 A, arg1 B, arg2 D) Z,
+	arg0 A
+) fn(B, D) Z
+{
+	return fn[predicate, arg0]<A, B, D, Z> (arg1 B, arg2 D) Z {
+		return predicate(arg0, arg1, arg2)
+	}
+}
--- curryN_test.v
+++ curryN_test.v
@@ -8,7 +8,6 @@ fn test_curry1of2() {
 	assert(new_fn(1) == '101')
 }
 
-/* curry1of3 fails with generics
 fn test_curry1of3() {
 	existing_fn := fn (arg0 int, arg1 int, arg2 int) string {
 		return (arg0 + arg1 + arg2).str()
@@ -16,4 +15,3 @@ fn test_curry1of3() {
 	new_fn := r.curry1of3<int, int, int, string>(existing_fn, 100)
 	assert(new_fn(1, 2) == '103')
 }
-*/
