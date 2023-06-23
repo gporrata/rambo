e99c30e51d6a6e5d2c59cd2e84de4bb3ce39d2cb
George Porrata <george.porrata@tstllc.net>
2023-06-23 14:53:30 -0400

    ref: all_pass made generic

--- all_pass.v
+++ all_pass.v
@@ -1,9 +1,6 @@
 module rambo
 
-// TODO: making this generic fails, fix.
-
-// returns true only if all `predicates` return true given input
-pub fn all_pass(predicates []fn(val int) bool, input int) bool {
+pub fn all_pass<A>(predicates [] fn(val A) bool, input A) bool {
 	for predicate in predicates {
 		if ! predicate(input) { return false }
 	}
