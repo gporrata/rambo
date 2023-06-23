bbbaa0c1e53ee366c6e9e9376ca857e644c1e2e8
George Porrata <george.porrata@tstllc.net>
2023-06-23 14:54:16 -0400

    ref: any_pass made generic

--- any_pass.v
+++ any_pass.v
@@ -1,9 +1,6 @@
 module rambo
 
-// TODO: making this generic fails. should be able to use T for all ints
-
-// returns true if any of the `predicates` returns true given `input`
-pub fn any_pass(predicates []fn(t int) bool, input int) bool {
+pub fn any_pass<A>(predicates []fn(t A) bool, input A) bool {
 	for predicate in predicates {
 		if predicate(input) { return true }
 	}
